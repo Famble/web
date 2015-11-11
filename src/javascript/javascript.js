fdb8570595fd560cfdb08643982154eab4974ce9
Johan <josaks@online.no>
2015-11-11 15:04:55 +0100

    .

--- src/javascript/javascript.js
+++ src/javascript/javascript.js
@@ -1,6 +1,7 @@
 	var imagecount = 0;
 	var total = 8;
 		
+	/* endrer bilde når man trykker på pilene*/
 	function slide(v)
 	{
 		var Image = document.getElementById("img");
@@ -18,9 +19,17 @@
 		Image.src="../image/kart/etasje" + imagecount + ".gif";
 	}
 		
+	/* endrer bilde når man trykker i etasje-menyen*/
 	function setBilde(x)
 	{
 		var Image = document.getElementById("img");
 		
 		Image.src="../image/kart/etasje" + x + ".gif";
+	}
+	
+	/* åpner og lukker svar i FAQ-en */
+	function toggleFAQ(answer)
+	{
+		document.getElementById(answer).classList.toggle("vis");
+		document.getElementById(answer).classList.toggle("skjul");
 	}
